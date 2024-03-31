6c7f592eaf57a3388c5b591315ee0bc23907e85f
abhi-s-03 <abhisheks3002@gmail.com>
2024-03-31 15:32:06 +0530

    navbar added

--- src/components/Navbars.jsx
+++ src/components/Navbars.jsx
@@ -1,32 +1,20 @@
-
-import logo from '../assets/logo.png';
-import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from 'flowbite-react';
-
+import logo from "../assets/logo.png";
+import "./styles/navbar.css";
 function Navbars() {
-  if (NavbarLink.active === true) {
-    NavbarLink.className = 'text-white bg-black rounded-md px-2 py-1';
-  }
   return (
-    <Navbar fluid rounded className='flex flex-row'>
-      <NavbarBrand href="#" className='w-auto'>
-        <img src={logo} className="mr-3 h-16" alt="CSA Logo" />
-        
-      </NavbarBrand>
-      <NavbarToggle className='flex justify-end bg-white w-auto hover:bg-transparent  hover:border-transparent focus:ring-transparent outline:none ' />
-      <NavbarCollapse className='bg-white '>
-        <NavbarLink className='hover:text-slate-400' href="/" >
-          Home
-        </NavbarLink>
-        <NavbarLink className='hover:text-slate-400' href="/events">
-          Events
-        </NavbarLink>
-        
-        <NavbarLink className='hover:text-slate-400' href="/dotslash">Dotslash</NavbarLink>
-        <NavbarLink className='hover:text-slate-400' href="/team">Team</NavbarLink>
-
-        <NavbarLink className='hover:text-slate-400' href="/contact">Contact</NavbarLink>
-      </NavbarCollapse>
-    </Navbar>
+    <>
+      <div className="navbar">
+        <div className="nav-items">
+          <a href="#">Achievements</a>
+          <a href="#">Newsletter</a>
+          <a href="#">
+            <img src={logo} alt="Logo" />
+          </a>
+          <a href="#">Dotslash</a>
+          <a href="#">CS Cup</a>
+        </div>
+      </div>
+    </>
   );
 }
 
