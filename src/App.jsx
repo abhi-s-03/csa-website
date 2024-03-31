4e931d2b4ff81c9bb968776f9ff213404e13a4dc
abhi-s-03 <abhisheks3002@gmail.com>
2024-03-31 17:03:11 +0530

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import Navbars from "./components/Navbars.jsx";
-import Footers from "./components/Footers.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing.jsx";
 import Events from "./pages/Events.jsx";
 import Team from "./pages/Team.jsx";
 import Dotslash from "./pages/Dotslash.jsx";
 import Contact from "./pages/Contact.jsx";
-import './App.css'
+import "./App.css";
+
 function App() {
-  
   return (
     <>
-      <Navbars />
-      <div className="home"></div>
-      {/* <Footers /> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/dotslash" element={<Dotslash />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
