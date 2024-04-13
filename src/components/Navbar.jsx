e4ef2a8f99cbc30bf4d2d92619305b9507fa2697
riya461 <riyasabures@gmail.com>
2024-04-14 03:09:51 +0530

    changed the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../assets/logo.png';
-import './styles/navbar.css';
 
-function Navbar({ textColor }) {
+function Navbar({ textColor = 'white'}) {
   return (
     <>
-      <div className="navbar">
-        <div className="nav-items">
-          <NavLink to="/achievements" activeClassName="active">
-            Achievements
-          </NavLink>
-          <NavLink to="/dotslash" activeClassName="active">
-            Dotslash
-          </NavLink>
-          <NavLink to="/" activeClassName="active">
-            <img src={logo} alt="Logo" />
-          </NavLink>
-          <NavLink to="/cscup" activeClassName="active">
-            CS Cup
-          </NavLink>
-          <NavLink to="/newsletter" activeClassName="active">
-            Newsletter
-          </NavLink>
+      <nav className=" backdrop-filter backdrop-blur-lg fixed w-full z-10">
+        <div className="max-w-7xl mx-auto py-5 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex">
+              <NavLink className={`flex text-${textColor} hover:bg-gray-500 justify-center align-middle hover:bg-opacity-10 px-5 py-5 rounded-md text-xl`} to="/achievements" activeClassName="active">
+                Achievements
+              </NavLink>
+              <NavLink className={`flex text-${textColor} hover:bg-gray-500 justify-center align-center hover:bg-opacity-10 px-5 py-5 rounded-md text-xl`} to="/newsletter" activeClassName="active">
+                Newsletter
+              </NavLink>
+              <NavLink className={'flex  justify-center align-middle  '} to="/" activeClassName="active">
+                <img className='h-16 w-16' src={logo} alt="Logo" />
+              </NavLink>
+              <NavLink className={`flex text-${textColor} hover:bg-gray-500 justify-center align-middle hover:bg-opacity-10 px-5 py-5 rounded-md text-xl`} to="/dotslash" activeClassName="active">
+                DotSlash
+              </NavLink>
+              <NavLink className={`flex text-${textColor} hover:bg-gray-500 justify-center align-middle hover:bg-opacity-10 px-5 py-5 rounded-md text-xl`} to="/cscup" activeClassName="active">
+                CS Cup
+              </NavLink>
+            </div>
+          </div>
         </div>
-      </div>
+      </nav>
     </>
   );
 }
--- src/pages/Cscup.jsx
+++ src/pages/Cscup.jsx
@@ -1,3 +1,64 @@
-export default function Cscup() {
-  return <>CS Cup</>;
+
+import React, { useState } from 'react';
+import Navbar from "../components/Navbar";
+import image1 from '../assets/images/apples.jpg';
+import { IoIosArrowRoundForward, IoIosArrowRoundBack } from "react-icons/io";
+import cstitle from '../assets/images/image 39.svg';
+import './styles/cscup.css'
+
+function CsCup() {
+  const [selectedImage, setSelectedImage] = useState(0);
+  const images = [image1, image1, image1, image1, image1, image1, image1];
+
+  const handleClick = (index) => {
+    setSelectedImage(index);
+  };
+
+  const handleNext = () => {
+    setSelectedImage((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const handlePrev = () => {
+    setSelectedImage((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
+  return (
+    <div className='cs-full'>
+      <Navbar />
+      <div className='bg-gradient-to-b pt-40 pb-16 min-h-screen' style={{
+        background: ``,
+        
+      }}>
+        <div className='flex flex-row justify-center text-[#ffffff] font-bold'>
+        <img src={cstitle} alt="DotSlash Title" className="mx-auto mt-6 w- h-16" />
+        </div>
+        <div className='text-[41px] font-semibold text-center mt-6 text-[#ffffff]'>Gallery</div>
+        <div className='flex flex-row justify-center'>
+          <button onClick={handlePrev} ><IoIosArrowRoundBack className='m-0 text-5xl text-white' /></button>
+          {images.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt={'Image ' + (index + 1)}
+              style={{
+                height: '400px',
+                width: selectedImage === index ? '400px' : '100px',
+                backgroundPosition: selectedImage === index ? 'center' : 'left',
+                backgroundSize: selectedImage === index ? 'cover' : 'auto',
+                border: selectedImage === index ? '1px solid white' : '1px solid white',
+                margin: '0 5px',
+                cursor: 'pointer',
+                transition: 'width 0.5s'
+              }}
+              className='rounded-md'
+              onClick={() => handleClick(index)}
+            />
+          ))}
+          <button onClick={handleNext} className='m-0'><IoIosArrowRoundForward className='ml-0 text-5xl text-left text-white' /></button>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default CsCup;
