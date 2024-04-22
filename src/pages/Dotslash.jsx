6c086f18a49a112e271b0db171f2fabfc922a42c
riya461 <riyasabures@gmail.com>
2024-04-22 15:24:29 +0530

    fix

--- src/pages/Dotslash.jsx
+++ src/pages/Dotslash.jsx
@@ -14,7 +14,7 @@ import img9 from "../assets/images/dotslash/9.jpg";
 import './styles/dotslash.css';
 import Footer from '../components/Footer';
 // import AOS from 'aos';
-// import 'aos/dist/aos.css';
+import 'aos/dist/aos.css';
 import { useScroll } from 'framer-motion';
 
 function Gallery() {
